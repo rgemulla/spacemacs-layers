79fbc53ed2c40ab2fd9cf8c19ea13eb3557cdd69
Rainer Gemulla <rgemulla@uni-mannheim.de>
2024-02-15 17:08:43 +0100

    Back to lsp

--- +lang/latexp/layers.el
+++ +lang/latexp/layers.el
@@ -1,5 +1,4 @@
 (configuration-layer/declare-layers
  '(
-   ;; use auctex due to current LSP issues (Feb 2024)
-   (latex :variables latex-backend 'company-auctex latex-enable-folding t)
+   (latex :variables latex-backend 'lsp latex-enable-folding t)
  ))
