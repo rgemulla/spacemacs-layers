f32e929a92f3603a2793ae5b69f813e206c094a2
Rainer Gemulla <rgemulla@uni-mannheim.de>
2024-12-06 00:16:29 +0100

    Set weight to normal in latexp/font-latex-sedate-math-face

--- +lang/latexp/config.el
+++ +lang/latexp/config.el
@@ -1,4 +1,4 @@
 (defface latexp/font-latex-sedate-math-face
-  '((t :inherit font-latex-math-face :foreground "#8b4593")) ;; SaddleBrown = #8b4513
+  '((t :inherit font-latex-math-face :weight 'normal :foreground "#8b4593")) ;; SaddleBrown = #8b4513
   "Face used to highlight sedate commands in math mode."
   :group 'font-latex-highlighting-faces)
